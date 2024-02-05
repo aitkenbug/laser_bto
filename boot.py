37821551cf15ad1ed6075c4805e693c26d304931
aitkenbug <111537293+aitkenbug@users.noreply.github.com>
2024-02-05 15:45:29 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,14 +1,14 @@
 import machine, neopixel
 import time
 
-LED_POWER = machine.Pin(19, mode = machine.Pin.OUT, value = 1)
+LED_POWER = machine.Pin(17, mode = machine.Pin.OUT, value = 1)
 
 np = neopixel.NeoPixel(machine.Pin(18), 1)
 
 while True:
   np[0] = (255,255,255)
   np.write()
-  time.sleep(1)
+  time.sleep(2)
   np[0] = (0,0,0)
   np.write()
-  time.sleep(1)
+  time.sleep(2)
