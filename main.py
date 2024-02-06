6e3ba2f749cad794650f0569f9c43701a051a6f1
aitkenbug <111537293+aitkenbug@users.noreply.github.com>
2024-02-06 09:13:01 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import machine, neopixel
+import time
+
+
+LED_POWER = machine.Pin(17, mode = machine.Pin.OUT, value = 1)
+
+np = neopixel.NeoPixel(machine.Pin(18), 1)
+
+while True:
+  np[0] = (255,255,255)
+  np.write()
+  time.sleep(0.5)
+  np[0] = (0,0,0)
+  np.write()
+  time.sleep(0.5)
