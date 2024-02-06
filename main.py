4506254390296ea09d3196da173147b3d6742cae
aitkenbug <111537293+aitkenbug@users.noreply.github.com>
2024-02-06 10:34:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import time
 import network
 from secrets import secrets
 
-
-sta_if = network.WLAN(network.AP_IF)
-sta_if.active(True)
-sta_if.connect(secrets['ssid'], secrets['password'])
-while ap.active() == False:
-  pass
-
-
-
 LED_POWER = machine.Pin(17, mode = machine.Pin.OUT, value = 1)
 
 np = neopixel.NeoPixel(machine.Pin(18), 1)
