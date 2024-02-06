29f3c35481b19c1c1c7da01c6470b5021e3d7517
aitkenbug <111537293+aitkenbug@users.noreply.github.com>
2024-02-06 09:15:13 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,15 +1,2 @@
-import machine, neopixel
-import time
-
-
-LED_POWER = machine.Pin(17, mode = machine.Pin.OUT, value = 1)
-
-np = neopixel.NeoPixel(machine.Pin(18), 1)
-
-while True:
-  np[0] = (255,255,255)
-  np.write()
-  time.sleep(0.5)
-  np[0] = (0,0,0)
-  np.write()
-  time.sleep(0.5)
+import ugit
+ugit.pull_all()
